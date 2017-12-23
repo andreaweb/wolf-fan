ae5faf56fe4c5bbc8088832ee4c6fbca3b0a6359
Andrea Santana <deiainluv@gmail.com>
2017-12-23 16:27:10 -0200

    wallpaper added

--- src/components/homePage.js
+++ src/components/homePage.js
@@ -3,7 +3,9 @@ import React, { Component } from 'react';
 class Homepage extends Component {
   render() {
     return (
-      <h1>Wolf Fan</h1>
+    	<div className="bg">
+    		<h1>Wolf Fan</h1>
+    	</div>
     );
   }
 }
