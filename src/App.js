2d1f7c98f0bf6a43ae758f0fe016d630d09cb27a
Andrea Santana <deiainluv@gmail.com>
2017-12-24 15:21:19 -0200

    next page basic layout + react router

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import { Switch, Route, Link } from 'react-router-dom';
 import Homepage from './components/homePage.js';
+import Thumbpage from './components/thumbPage.js';
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <h1>Wolf Fan</h1>
-        </header>
-        <Homepage />
+        <Switch>
+          <Route exact path='/' component={Homepage} />
+          <Route path='/thumbPage' component={Thumbpage} />
+        </Switch>
       </div>
     );
   }
--- src/components/homePage.js
+++ src/components/homePage.js
@@ -1,11 +1,26 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class Homepage extends Component {
   render() {
     return (
-    	<div className="bg">
-    		
-    	</div>
+    	<div>
+	    	<header className="App-header">
+	          <h1>Wolf Fan</h1>
+	          <h2>Start or Join a Pack</h2>
+	          <p>Pay homage to your favorite artist. Memorize their songs and listen to them in repeat.
+	          Customize your own app all about them.</p>
+	        </header>
+    		<div className="bg">
+    		</div>
+    		<aside>
+    			<input placeholder="Band or Singer" type="search"></input>
+    			<Link to="/thumbPage">
+	    			<button>Go</button>
+    			</Link>
+    		</aside>
+    	
+		</div>
     );
   }
 }
