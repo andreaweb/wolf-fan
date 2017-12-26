f0c1171c9c90aa02e45ecf876f2e018f4294dd7a
Andrea Santana <deiainluv@gmail.com>
2017-12-26 11:28:13 -0200

    google image api still not working

--- src/components/googleSearch.js
+++ src/components/googleSearch.js
@@ -1,15 +1,35 @@
+/* global gapi */
 import React, { Component } from 'react';
 
+const API_KEY = 'AIzaSyCDaNYrEbiJSMlElJhOPsQHLs1u6TcP8R4';
+class Googlesearch extends Component {
 
-class Googlesearch extends React.Component {
+  loadYoutubeApi() {
+    const script = document.createElement("script");
+    script.src = "https://apis.google.com/js/client.js";
+
+    script.onload = () => {
+      gapi.load('client', () => {
+        gapi.client.setApiKey(API_KEY);
+        gapi.client.load('youtube', 'v3', () => {
+          this.setState({ gapiReady: true });
+        });
+      });
+    };
+
+    document.body.appendChild(script);
+  }
+
+  componentDidMount() {
+    this.loadYoutubeApi();
+  }
+
   render() {
-    return(
-      <div className = "content">
-        <div className="gcse-searchbox" data-resultsUrl="http://www.google.com" data-newWindow="true" data-queryParameterName="search" >
-        </div>
-      </div>
-    );
+      if (this.state.gapiReady) {
+       return (
+         <h1>GAPI is loaded and ready to use.</h1>
+       );
+    };
   }
 }
-
 export default Googlesearch;
--- src/components/thumbPage.js
+++ src/components/thumbPage.js
@@ -2,16 +2,6 @@ import React, { Component } from 'react';
 import Googlesearch from '../components/googleSearch.js';
 
 class Thumbpage extends Component {
-    componentWillMount(){
-        return (
-            (function() {
-                var cx = '123:456'; // Insert your own Custom Search engine ID here
-                var gcse = document.createElement('script'); gcse.type = 'text/javascript'; gcse.async = true;
-                gcse.src = 'https://cse.google.com/cse.js?cx=' + cx;
-                var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(gcse, s);
-            })()
-        ) 
-    }
     render() {
         return (
         	<div id="testgoogle">
