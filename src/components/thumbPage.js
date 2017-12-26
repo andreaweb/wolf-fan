f7a077e36ac3409f89f4de27cf9a37ee254526ac
Andrea Santana <deiainluv@gmail.com>
2017-12-26 09:08:58 -0200

    havia esquecido de add estes arquivos

---
+++ src/components/thumbPage.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class Thumbpage extends Component {
+  render() {
+    return (
+    	<div>
+            <header className="App-header">
+              <h1>Choose an image</h1>
+              <h2>to represent them best</h2>
+              <p>The image is gonna be your app thumbnail.</p>
+            </header>
+    		<div className="bg">
+    		</div>
+		</div>
+    );
+  }
+}
+
+export default Thumbpage;
